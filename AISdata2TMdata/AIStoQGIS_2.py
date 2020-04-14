74a3c19d4f1f571fa308e96352db361bfceca713
Cesar <cesainzl@gmail.com>
2020-04-14 13:57:46 +0200

    20200414_C

--- AISdata2TMdata/AIStoQGIS_2.py
+++ AISdata2TMdata/AIStoQGIS_2.py
@@ -5,11 +5,12 @@ import pandas as pd
 import glob
 import easygui as eg
 import os
-  
+
+dir ="D:/GitHub Project/AEMET/AISdata2TMdata"
 extension = ["*.csv"]
 mycsv = eg.fileopenbox(msg="Abrir archivo",
                          title="Control: fileopenbox",
-                         default='*.csv',
+                         default='dir/*.csv',
                          filetypes=extension)
 #print(mycsv)
 #eg.msgbox(mycsv, "fileopenbox", ok_button="Continuar")
@@ -20,7 +21,7 @@ mycsv3 = mycsv1[1]
 
 df = pd.read_csv(mycsv,sep=";",decimal=",",encoding="windows-1252",parse_dates=["Timestamp"])
 #print(df)
-mycsv4 = df.to_csv("TIME_MANAGER_"+ mycsv3)
+mycsv4 = df.to_csv("D:/GitHub Project/AEMET/AISdata2TMdata/"'TIME_MANAGER_'+ mycsv3)
 #print(mycsv2)
 mycsv4 = "TIME_MANAGER_{}   ".format(mycsv3)
 mycsv5 = "PROCESADO  FILE {} => TIME_MANAGER_{}   ".format(mycsv3,mycsv3)
